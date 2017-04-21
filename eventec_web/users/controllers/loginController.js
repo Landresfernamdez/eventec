cd4dba428a60cee496f69b05853f12aa2a1b4110
Andresf12 <landresf3638@gmail.com>
2017-04-21 08:45:18 -0600

    actualizacion

---
+++ eventec_web/users/controllers/loginController.js
@@ -0,0 +1,50 @@
+angular.module('loginModule',["ngRoute","ngResource"])
+    .controller('loginController', function($scope,$http) {
+
+        // modelo de datos.
+        $scope.ida = "";
+        $scope.pass = "";
+        var user={
+                     id:null
+                    };
+        /**
+         * Ejecuta el inicio de sesión.
+         */
+         
+        $scope.doLogin = function () {
+            user.id=$scope.ida;
+            saveSession(user);
+            console.log(user);
+            console.log(localStorage.getItem("user"));
+            var ida=Base64.encode($scope.ida);
+            var pass=Base64.encode($scope.pass);
+            console.log(ida);
+            console.log(pass);
+
+            $http({
+                method:"GET",//
+                url: "http://172.24.40.241/Administradores/Administrador?ida="+ida+"&pass="+pass
+            }).then(function mySucces(response){
+                var estado=response.data;
+                if(estado.success){
+                    console.log(response.data);
+                    window.location.href = ('users/MainView.html');
+                }
+                else{
+                    alert("Credenciales incorrectas");
+                    console.log("Credenciales incorrectas");
+                }
+                
+            });
+        }
+        
+
+        /**
+         * Guarda la sesión en el almacenamiento local del navegador.
+         * @param json JSON de origen.
+         */
+        function saveSession(json) {
+            localStorage.setItem("session.user", json.id);
+            console.log("Sesión guardada.");
+        }
+    });
---
+++ eventec_web/users/controllers/main.js
@@ -0,0 +1,7 @@
+angular.module('mainModule',["ngRoute","ngResource"])
+.controller('mainController',function($scope,$http){
+	$scope.jumpGetevents=function(){
+            window.location.href=('/events/views/getEvents');
+                                    };
+	
+});
